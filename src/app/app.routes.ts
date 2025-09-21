693545611e990ebeea36b99296a3e7f953192a49
matheus.alvz <matheus.oliveira@grupohne.com.br>
2025-09-21 12:56:16 -0300

    Redefine as rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { AlunosListComponent } from './components/alunos-list/alunos-list.component';
 import { AlunoFormComponent } from './components/alunos-form/aluno-form.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 export const appRoutes: Routes = [
-    { path: '', component: AlunosListComponent },
+    // { path: '', component: AlunosListComponent },
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
     { path: 'aluno', component: AlunoFormComponent },
-    { path: '**', redirectTo: '' }
+    { path: 'alunos', component: AlunosListComponent },
+    { path: '**', redirectTo: 'login' }
 ];  
